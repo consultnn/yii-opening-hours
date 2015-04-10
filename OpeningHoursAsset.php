ddfb82d25b221fa1899123fdcb7ee33d7c885d0a
Leksanov Artem <52lexanov@gmail.com>
2015-04-10 12:40:38 +0300

    asset update

--- OpeningHoursAsset.php
+++ OpeningHoursAsset.php
@@ -10,10 +10,10 @@ class OpeningHoursAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/openingHours.css',
+        'assets/openingHours.css',
     ];
     public $js = [
-        'js/openingHours.js',
+        'assets/openingHours.js',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
