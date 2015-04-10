e4c5a736eed84d908d9d4136886ac005304b94b3
Leksanov Artem <52lexanov@gmail.com>
2015-04-10 14:07:54 +0300

    asset update

--- OpeningHoursAsset.php
+++ OpeningHoursAsset.php
@@ -8,7 +8,7 @@ use yii\web\AssetBundle;
 class OpeningHoursAsset extends AssetBundle
 {
     public $basePath = '@vendor/consultnn/yii2-opening-hours/assets';
-    //public $baseUrl = '@web';
+    public $baseUrl = '@web';
     public $css = [
         'openingHours.css',
     ];
